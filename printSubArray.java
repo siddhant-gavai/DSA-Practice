84233421900a5171f3a1df025c0e1e2352319e46
Siddhant Gavai <siddhantgavai0@gmail.com>
2025-11-17 09:34:23 +0530

    Practice printSubArray

---
+++ printSubArray.java
@@ -0,0 +1,23 @@
+public class printSubArray {
+
+    public static void subArray(int[] numbers) {
+        int totalSubArray = 0;
+        for (int i = 0; i < numbers.length; i++) {
+            for (int j = i; j < numbers.length; j++) {
+                for (int k = i; k <= j; k++) {
+                    System.out.print(numbers[k] + " ");
+
+                }
+                totalSubArray++;
+                System.out.println();
+            }
+            System.out.println();
+        }
+        System.out.println("Total SubArrays: " + totalSubArray);
+    }
+
+    public static void main(String[] args) {
+        int[] numbers = { 2, 4, 6, 8, 10 };
+        subArray(numbers);
+    }
+}
