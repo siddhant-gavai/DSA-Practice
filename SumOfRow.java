ee1a864346af5c119a4ad361bdd445a4127f1bcd
Siddhant Gavai <siddhantgavai0@gmail.com>
2025-11-19 08:12:29 +0530

    Add SumOfRow.java — sum of each row solution

---
+++ SumOfRow.java
@@ -0,0 +1,18 @@
+public class SumOfRow {
+
+    public static void main(String[] args) {
+        int[][] numbers = {
+                { 1, 4, 9 },
+                { 11, 4, 3 },
+                { 2, 2, 3 }
+        };
+
+        int sum = 0;
+
+        for (int j = 0; j < numbers[0].length; j++) {
+            sum = sum + numbers[1][j];
+        }
+
+        System.out.println(sum);
+    }
+}
